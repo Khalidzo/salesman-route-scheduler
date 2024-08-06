fd459de61ec799f79e02f7099220098c64a1c03c
unknown <kh.salama2001@gmail.com>
2024-08-06 15:00:02 +0200

    input form

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from "@angular/router";
+import { HomePageComponent } from "src/components";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    component: HomePageComponent,
+    path: ""
+  }
+];
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        semiBlack: "#1e1e1e",
+        lightSemiBlack: "#3c3c3c"
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"]
+      }
+    }
   },
   plugins: []
 };
